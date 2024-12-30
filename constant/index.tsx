09fdeed5ce98116a21e9674c5ea15e4f13f7acf8
TokenTechiePro <joshua.diaz.prodev@gmail.com>
2024-12-31 05:37:25 +0800

    fix: social contract infors

--- constant/index.tsx
+++ constant/index.tsx
@@ -1,21 +1,21 @@
 export const Social_Icons = [
+  // {
+  //   link: "https://github.com/ProScriptSlinger",
+  //   image: "/Github.svg",
+  //   alt: "Github",
+  // },
   {
-    link: "https://github.com/ProScriptSlinger",
-    image: "/Github.svg",
-    alt: "Github",
-  },
-  {
-    link: "https://www.facebook.com/profile.php?id=61561674492097",
+    link: "https://www.facebook.com/profile.php?id=615616744920921",
     image: "/facebook.svg",
     alt: "facebook",
   },
   {
-    link: "https://www.linkedin.com/in/abel-tony-998776318",
+    link: "https://www.linkedin.com/in/isabella-james",
     image: "/LinkedIn.svg",
     alt: "LinkedIn",
   },
   {
-    link: "https://x.com/AbelTony31 ",
+    link: "https://x.com/isabella-james ",
     image: "/Twitter.svg",
     alt: "Twitter",
   },
